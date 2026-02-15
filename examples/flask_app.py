a829a11c417a74735a7daabcf977f35064415185
kkamal11 <kishorkamal7091@gmail.com>
2026-02-15 12:28:50 +0530

    flask-app-template-test

--- examples/flask_app.py
+++ examples/flask_app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello from Flask running on custom server!"
+    return render_template("home.html")
+
+
+@app.route("/second-page")
+def second_page():
+    return render_template("second_page.html")
